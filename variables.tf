36da538efd6891412ec91e244652869e4ff3e070
Gokul Mohanraj <thegokulraj@gmail.com>
2025-02-11 14:58:41 +0530

    Changes made in variable.tf file

--- variables.tf
+++ variables.tf
@@ -3,18 +3,3 @@ variable "instance_type" {
   type        = string
   default     = "t2.micro"
 }
-
-variable "access_key" {
-  description = "Access key for aws"
-  type        = string
-}
-
-variable "secret_key" {
-  description = "secret key foe aws"
-  type        = string
-}
-
-variable "email" {
-  description = "email for subscription"
-  type        = string
-}
