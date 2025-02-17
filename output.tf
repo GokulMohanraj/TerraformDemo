310b1da9fda5f532b4c54eef96ecf1a36c2884f1
Gokul Mohanraj <thegokulraj@gmail.com>
2025-02-17 16:01:25 +0400

    Update in output.tf file

--- output.tf
+++ output.tf
@@ -7,3 +7,9 @@ output "instance_ip" {
   description = "This is aws instance public ip"
   value       = aws_instance.demo.public_ip
 }
+
+output "aws_s3_bucket" {
+  description = "This is aws s3 bucket name"
+  value       = aws_s3_bucket.demo-bucket.bucket
+  
+}
